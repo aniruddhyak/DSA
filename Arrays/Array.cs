ca45d35378b3baebe8b25a593e78274cf0acbb99
Aniruddhya Khatua <aniruddhyak@gmail.com>
2024-11-17 15:13:49 -0800

    Update Array.cs

--- Arrays/Array.cs
+++ Arrays/Array.cs
@@ -1,1 +1,33 @@
-To track all ARRAY related solutions
+/// <summary>
+/// SORT COLORS
+/// 3 POINTERS APPROACH
+/// </summary>
+/// <param name="arr"></param>
+public static void SortColor(int[] arr)
+    {
+        int leftPtr = 0, middlePtr = 0, rightPtr = arr.Length - 1;
+
+        while (middlePtr < rightPtr)
+        {
+            if (arr[middlePtr] == 0)
+            {
+                int temp = arr[middlePtr];
+                arr[middlePtr] = arr[leftPtr];
+                arr[leftPtr] = temp;
+                leftPtr++;
+                middlePtr++;
+            }
+
+            else if (arr[middlePtr] == 1)
+                middlePtr++;
+
+            else if (arr[middlePtr] == 2)
+            {
+                int temp = arr[middlePtr];
+                arr[middlePtr] = arr[rightPtr];
+                arr[rightPtr] = temp;
+                rightPtr--;
+            }
+
+        }
+    }
